4570eceb22f36358d69463119e453024066833ed
Anton <bolychev.anthon@yandex.ru>
2025-05-26 21:03:50 +0300

    feat: auto commit

--- src/envs/lidarnav.py
+++ src/envs/lidarnav.py
@@ -112,7 +112,7 @@ class LidarNavEnv(gym.Env):
         done = False
 
         dist_to_goal = np.linalg.norm(self.robot_pos - self.goal_pos)
-        reward = 0.1 * dist_to_goal
+        reward = - 0.1 * dist_to_goal
         if dist_to_goal < self.goal_radius:
             reward += 1.0
             done = True
