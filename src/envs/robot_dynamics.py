49f8ca952972d12e0dc0b941ac27082b21855883
Anton <bolychev.anthon@yandex.ru>
2025-12-25 17:28:59 +0300

    feat: auto commit

--- src/envs/robot_dynamics.py
+++ src/envs/robot_dynamics.py
@@ -81,7 +81,7 @@ class RobotDynamicsEnv(gym.Env[np.ndarray, np.ndarray]):
                 -1.0,
                 self.config.world_low - self.config.world_high,
                 self.config.world_low - self.config.world_high,
-                0.0,
+                # 0.0,
             ],
             dtype=np.float32,
         )
@@ -93,7 +93,7 @@ class RobotDynamicsEnv(gym.Env[np.ndarray, np.ndarray]):
                 1.0,
                 self.config.world_high - self.config.world_low,
                 self.config.world_high - self.config.world_low,
-                1.0,
+                # 1.0,
             ],
             dtype=np.float32,
         )
@@ -213,7 +213,7 @@ class RobotDynamicsEnv(gym.Env[np.ndarray, np.ndarray]):
                 self.robot_position[1] - self.target_position[1]
                 if self.collectable_captured
                 else self.robot_position[1] - self.collectable_position[1],
-                self.collectable_captured,
+                # self.collectable_captured,
             ],
             dtype=np.float32,
         )
