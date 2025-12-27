ee064a0322c2e7dbc1c597a67cb27b408a76974a
Anton <bolychev.anthon@yandex.ru>
2025-12-27 20:09:29 +0300

    feat: auto commit

--- src/envs/robot_dynamics.py
+++ src/envs/robot_dynamics.py
@@ -234,7 +234,9 @@ class RobotDynamicsMetricsCollector(MetricsCollector):
         self.rolling_window_size = rolling_window_size
 
     def collect_env_related_metrics_from_info(self, info: dict, step: int):
+        print("collect_env_related_metrics_from_info")
         super().collect_env_related_metrics_from_info(info, step)
+        print("after super().collect_env_related_metrics_from_info")
         self.append_metric(
             "episode_stats/distance_to_target", info["distance_to_target"], step=step
         )
